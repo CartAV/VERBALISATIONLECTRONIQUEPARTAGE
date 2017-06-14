82aae897e0938e6d73fe0bd7d0d3939e7ca4654c
grabenertr <grabenertr>
2017-06-14 15:42:54 +0200

    Saved recipe: VERBALISATIONLECTRONIQUEPARTAGE.compute_2010_2015_pve_pg_prep

--- recipes/compute_2010_2015_pve_pg_prep.sql
+++ recipes/compute_2010_2015_pve_pg_prep.sql
@@ -9,7 +9,7 @@ FROM "2010_2015_pve_sr_pg" as pve
 
 LEFT JOIN LATERAL (SELECT "INSEE_COM", num_route_or_id, num_route_com_id, st_distance(st_point(pve.result_longitude, pve.result_latitude), the_geom) as distance, geojson
      FROM "osm_routes_par_commune_geojson"  as routes
-     where routes.the_geom && st_point(result_longitude, result_latitude)
+     where routes.the_geom && st_point(longitude, latitude)
      ORDER BY distance
     LIMIT 1) as nearest_route
 ON true
