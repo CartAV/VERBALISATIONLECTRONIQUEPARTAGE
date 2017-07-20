8ded8c274cd8edf8d8921e713f9a1e889d01152b
chabouisfr <chabouisfr>
2017-07-20 11:15:15 +0200

    Saved recipe: VERBALISATIONLECTRONIQUEPARTAGE.compute_2010_2015_pve_autoroute_coords

--- recipes/compute_2010_2015_pve_autoroute_coords.sql
+++ recipes/compute_2010_2015_pve_autoroute_coords.sql
@@ -1,2 +1,8 @@
-SELECT *
-  FROM "2010_2015_pve_prep_autoroute"
+SELECT DISTINCT ON (pve."PVE_ID") 
+    pve.*, b.lat as lat_pr, b.lon as lon_pr, '1.0' as pr_score
+    FROM "2010_2015_pve_prep_autoroute" pve
+    LEFT JOIN "bornes_routes_2016_commune" b
+    ON
+        (pve."CODE_INSEE_INFRACTION" = b."INSEE_COM")
+        AND (pve."LIBELLE_VOIE_RN" = b."nom_courant_route")
+        AND (pve."LIBELLE_TYPE_VOIE_DEDUIT" = 'Autoroute' or pve."LIBELLE_TYPE_VOIE_DEDUIT" = 'Route Nationale')
