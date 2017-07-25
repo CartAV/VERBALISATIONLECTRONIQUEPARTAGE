e2511328ab3adf2becbb72a7381dd5e251592c44
grabenertr <grabenertr>
2017-07-25 15:15:45 +0200

    Saved recipe: VERBALISATIONLECTRONIQUEPARTAGE.compute_pve_geocoded_distance_commune

--- recipes/compute_pve_geocoded_distance_commune.sql
+++ recipes/compute_pve_geocoded_distance_commune.sql
@@ -1,2 +1,8 @@
-SELECT *
+SELECT 
+    "2010_2015_pve_geocoded".*,
+    st_distance("IGN_COMMUNE_FRANCE".the_geom, st_point(longitude, latitude)) as distance_commune,
+
+    "IGN_COMMUNE_FRANCE"."CODE_COM" AS "CODE_COM"
   FROM "2010_2015_pve_geocoded"
+  LEFT JOIN "ign_commune_france" "IGN_COMMUNE_FRANCE"
+    ON "2010_2015_pve_geocoded"."CODE_INSEE_INFRACTION" = "IGN_COMMUNE_FRANCE"."INSEE_COM"
