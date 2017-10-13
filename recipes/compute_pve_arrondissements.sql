4e1cd4f2431243c636328ef0a4eea671241f8b20
grabenertr <grabenertr>
2017-10-13 12:08:22 +0200

    Saved recipe: VERBALISATIONLECTRONIQUEPARTAGE.compute_pve_arrondissements

--- recipes/compute_pve_arrondissements.sql
+++ recipes/compute_pve_arrondissements.sql
@@ -3,5 +3,5 @@ SELECT *, ign_commune_france."INSEE_COM" as code_arrondissement
   LEFT JOIN ign_commune_france
   ON st_within(st_SetSrid(st_point(longitude, latitude), 4326), "the_geom"::geometry)
   AND "CODE_INSEE_INFRACTION" IN ('75056', '69123', '13055')
-  AND "CODE_DEPT" IN ('75', '69', '13')
-  AND "INSEE_COM" NOT IN ('75056', '69123', '13055')
+  AND "2010_2015_pve_sr_regions_postgis"."CODE_DEPT" IN ('75', '69', '13')
+  AND "2010_2015_pve_sr_regions_postgis"."INSEE_COM" NOT IN ('75056', '69123', '13055')
